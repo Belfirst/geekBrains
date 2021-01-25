98c1533aa78b05c37589012f18d7490e608d3c1c
Belf <sleven182@mail.ru>
2021-01-25 11:18:13 +0300

    Add Lesson7

--- src/main/java/FirstApp.java
+++ src/main/java/FirstApp.java
@@ -1,4 +1,25 @@
 public class FirstApp {
+    public static void main(String[] args) {
 
+        Cat[] catArray;
+        catArray = new Cat[5];
+        catArray[0] = new Cat("Barsik",10);
+        catArray[1] = new Cat("Fat",15);
+        catArray[2] = new Cat("Little",5);
+        catArray[3] = new Cat("Slow",10);
+        catArray[4] = new Cat("Looser",10);
 
+        Plate plate = new Plate(45);
+        plate.info();
+        for (Cat cat : catArray) {
+            cat.eat(plate);
+        }
+        plate.info();
+        plate.addFood(45);
+        plate.info();
+        for (Cat cat : catArray) {
+            cat.eat(plate);
+        }
+        plate.info();
+    }
 }
---
+++ src/main/java/Plate.java
@@ -0,0 +1,20 @@
+public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public boolean decreaseFood(int n) {
+        if(food >= n) {
+            food -= n;
+            return true;
+        }
+        else System.out.println("Еды втарелке недостаточно.");
+        return false;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+    public void addFood(int n) {
+        food += n;
+    }
+}
